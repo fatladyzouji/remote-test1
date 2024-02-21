94e5155b368d22cb6a5640ad176c90c3defa5cc8
zouj3 <87604883@qq.com>
2024-02-21 18:08:52 +0800

    Create common.java
    提交一个本地文件Common.java

---
+++ common.java
@@ -0,0 +1,25 @@
+public class Common{
+
+    private String commonId;
+
+    public String getCommonId(){
+        return commonId;
+    }
+
+    public void setCommonId(String commonId){
+        this.commonId = commonId;
+    }
+	
+	
+	private String commonName;		
+	
+
+    public String getCommonName(){
+        return commonName;
+    }
+
+    public void setCommonName(String commonName){
+        this.commonName = commonName;
+
+    }
+}
