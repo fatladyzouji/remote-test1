d8baf23e3d0a0769722f9c6ea32a6ed626d1d250
zouj3 <87604883@qq.com>
2024-02-21 18:16:40 +0800

    test new a directory
    测试放一个文件夹进去

---
+++ 测试放一个文件夹进去/user.java
@@ -0,0 +1,21 @@
+public class user {
+
+    private String user_id;
+    private String userchname;
+
+    public void setUser_id(String user_id){
+        this.user_id = user_id;
+    }
+
+    public String getUser_id(){
+        return this.user_id;
+    }
+
+    public void setUserchname(String userchname){
+        this.userchname = userchname;
+    }
+
+    public String getUserchname(){
+        return this.userchname;
+    }
+}
